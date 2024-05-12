5105c1bb348d6f20425246bf1e0dfb5c473640ef
11111ccccc <164731406+11111ccccc@users.noreply.github.com>
2024-05-12 17:48:08 +0900

    5/12 +

--- Assets/Scripts/start.cs
+++ Assets/Scripts/start.cs
@@ -2,13 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class start : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
